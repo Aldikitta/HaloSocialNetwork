da4ae729220c42897201a987a3da268a97e12c2b
Aldik <andi.kitta77@gmail.com>
2023-01-20 15:31:25 +0800

    fixing issue in libs.versions.toml v2

---
+++ build.gradle.kts
@@ -0,0 +1,13 @@
+
+buildscript {
+}
+
+plugins {
+    alias(libs.plugins.com.android.application) apply false
+    alias(libs.plugins.org.jetbrains.kotlin.jvm) apply false
+    alias(libs.plugins.org.jetbrains.kotlin.serialization) apply false
+    alias(libs.plugins.com.google.dagger.hilt.android) apply false
+
+    alias(libs.plugins.nl.littlerobots.version.catalog.update)
+    alias(libs.plugins.com.github.ben.manes.versions)
+}
